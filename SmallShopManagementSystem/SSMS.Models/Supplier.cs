a17aebf9a6e42a0783ac8f1b576677e5267db9a8
Shakikul Islam <shakikulislam625010@gmail.com>
2019-04-09 21:50:02 +0600

    Add Supplier Model

---
+++ SmallShopManagementSystem/SSMS.Models/Supplier.cs
@@ -0,0 +1,15 @@
+﻿namespace SSMS.Models
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Contact { get; set; }
+        public string ContactPerson { get; set; }
+        public byte[] Image { get; set; }
+
+    }
+}
---
+++ SmallShopManagementSystem/SmallShopManagementSystem/Controllers/SuppliersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SSMS.Models;
+
+namespace SmallShopManagementSystem.Controllers
+{
+    public class SuppliersController : Controller
+    {
+        public ActionResult Add()
+        {
+            return View();
+        }
+        public ActionResult Add(Supplier supplier)
+        {
+            return View();
+        }
+	}
+}
