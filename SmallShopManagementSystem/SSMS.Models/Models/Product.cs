af6c2f0b3af6603c16934bca09f14ebfb549772d
Shimleen <shimleen@gmail.com>
2019-04-18 11:52:42 +0600

    Add_DropDown

--- SmallShopManagementSystem/SSMS.Models/Models/Product.cs
+++ SmallShopManagementSystem/SSMS.Models/Models/Product.cs
@@ -2,9 +2,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace SSMS.Models.Models
 {
@@ -14,14 +16,15 @@ namespace SSMS.Models.Models
 
         public string Code { get; set; }
         public string Name { get; set; }
-        public Category Category { get; set; }
-
-       // public string Category { get; set; }
+        public virtual Category Category { get; set; }
+        public int CategoryId { get; set; }
 
         [DisplayName("Reorder Level")]
         public int ReorderLevel { get; set; }
         public string Description { get; set; }
         public byte[] Image { get; set; }
+        [NotMapped]
+        public List<SelectListItem> CategoryLookUp { get; set; }
 
     }
 }
